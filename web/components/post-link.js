97866daf7d3ebd4ea1d3fbe665bd7ce3e30da8d5
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-01-23 17:28:06 +0000

    post link

--- web/components/post-link.js
+++ web/components/post-link.js
@@ -1,13 +1,18 @@
 import Link from 'next/link'
+import BlockContent from '@sanity/block-content-to-react'
+import client from '../client'
 
-const PostLink = (props) => {  
-  const { slug, title } = props.props
+const PostLink = ({props}) => {  
+  const { slug, title, summary} = props
 
-  console.log(slug)
   return (
     <Link href={{pathname: "/posts/[slug]"}} as={`/posts/${slug.current}`} >
       <a>
         <h1>{title}</h1>
+        <BlockContent
+          blocks={summary}
+          {...client.config()}
+        />
       </a>
     </Link>
   )
