d3766cbfc6a1ab081d5ae118a60655c6766416f4
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-01-25 13:35:02 +0000

    context

--- web/pages/posts/[slug].js
+++ web/pages/posts/[slug].js
@@ -40,6 +40,7 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
 }`
 
 Post.getInitialProps = async function (context) {
+  console.log(context)
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = "" } = context.query
   return await client.fetch(query, { slug })
