3f755dbe731e85c624ae047becb1acfc35dfe1e2
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-01-25 14:53:28 +0000

    export

--- web/next.config.js
+++ web/next.config.js
@@ -20,28 +20,31 @@ module.exports = {
   };
 
 module.exports = withOffline(
-  withSass({  }, {
-    exportPathMap: async function (defaultPathMap) {
-      const path = await client
-        // get all the posts and return those with slugs
-        .fetch('*[_type == "post"].slug.current')
-        .then(data =>
-          // use reduce to build an object with routes
-          // and select the blog.js file, and send in the
-          // correct query paramater.
-          data.reduce(
-            (slug) => ({
-              '/': { page: '/' },
-              [`/blog/${slug}`]: { page: '/posts', query: { slug } }
-            }),
-            {}
+  withSass({ 
+      // Make sure that your node enviroment supports async/await
+      exportPathMap: async function (defaultPathMap) {
+      
+        const path = await client
+          // get all the posts and return those with slugs
+          .fetch('*[_type == "post"].slug.current')
+          .then(data =>
+            // use reduce to build an object with routes
+            // and select the blog.js file, and send in the
+            // correct query paramater.
+            data.reduce(
+              (acc, slug) => ({
+                '/': { page: '/' },
+                ...acc,
+                [`/posts/${slug}`]: { page: '/posts/[post]', query: { slug } }
+              }),
+              {}
+            )
           )
-        )
-        .catch(console.error)
-      return path
-    }
-  })
-)
+          .catch(console.error)
+        return path
+      }
+   })
+  )
 
 // module.exports = {
 //   // Make sure that your node enviroment supports async/await
