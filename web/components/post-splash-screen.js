e20c2a0a96c1599dd9ee226ea2766b25f8d111e9
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-01-29 18:47:02 +0000

    page

--- web/components/post-splash-screen.js
+++ web/components/post-splash-screen.js
@@ -2,9 +2,9 @@
 const PostSplashScreen = ({title, authorName, imageUrl, tagline}) => (
   <div className="c-screen u-pad-h-md container__row--v-center u-dis-flex" style={{backgroundImage: `url(${imageUrl})`}}>
     	<div className="container container__row--v-center flex-column-tablet  u-transition-in"> 
-				<div className="col-7@md col-12 container container__column u-t-cen">
-					<h1>{title}</h1>
-					<p className="h2">{tagline}</p>
+				<div className="col-7@md col-12 container container__column u-t-cen u-pad-v-md">
+					<h2>{title}</h2>
+					<p className="h3">{tagline}</p>
 				</div>
 			</div>
   </div>
