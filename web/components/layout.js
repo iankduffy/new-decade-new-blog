78a701fc5bbd96b0e86c84770e7a186049aa9226
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-01-29 18:13:19 +0000

    remove about temp

--- web/components/layout.js
+++ web/components/layout.js
@@ -8,18 +8,18 @@ export default ({ children, title = 'My Blog | Ian Duffy |  Web Developer' }) =>
     <Head>
       <title>{title}</title>
       <meta charSet='utf-8' />
-      <meta name="description" content="I am a Web Developer based in Leeds, West Yorkshire. Who enjoys designing and building websites, with multiple languages and tools. Currently working with Ruby on Rails site, with multiple side projects using vanilla Javascript, reactJS and other frameworks."/>
+      <meta name="description" content="Web Developer: What Do I Know? Do I Know Things?? Let's Find Out!"/>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name="theme-color" content="#223a57"/>
-      <meta property="og:title" content="Ian Duffy | Web Developer"/>
-      <meta property="og:description" content="I am a Web Developer based in Leeds, West Yorkshire. Who enjoys designing and building websites, with multiple languages and tools. Currently working with Ruby on Rails site, with multiple side projects using vanilla Javascript, reactJS and other frameworks."/>
+      <meta property="og:title" content="Blog | Ian Duffy | Web Developer"/>
+      <meta property="og:description" content="Web Developer: What Do I Know? Do I Know Things?? Let's Find Out!"/>
       <meta property="og:image" content="https://www.iankduffy.com/iankduffy-socialcard.jpg"/>
       <meta property="og:url" content="https://www.iankduffy.com/"/>
-      <meta name="twitter:title" content="Ian Duffy | Web Developer"/>
-      <meta name="twitter:description" content="I am a Web Developer based in Leeds, West Yorkshire. Who enjoys designing and building websites, with multiple languages and tools. Currently working with Ruby on Rails site, with multiple side projects using vanilla Javascript, reactJS and other frameworks."/>
+      <meta name="twitter:title" content="My Blog | Ian Duffy | Web Developer"/>
+      <meta name="twitter:description" content="A Web Developer: What Do I Know? Do I Know Things?? Let's Find Out!"/>
       <meta name="twitter:image" content="https://www.iankduffy.com/iankduffy-socialcard.jpg"/>
       <meta name="twitter:card" content="summary_large_image"/>
-      <meta name="twitter:image:alt" content="Ian Duffy | Web Developer"/>
+      <meta name="twitter:image:alt" content="My Blog | Ian Duffy | Web Developer"/>
       <link rel="manifest" href="/manifest.json"></link>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
     </Head>
