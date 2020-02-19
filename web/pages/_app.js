58f965418b79c7606da03105cc46265b57d3dae8
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-02-19 20:17:15 +0000

    google analytics

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,19 @@
 import App from 'next/app'
 import Layout from '../components/layout'
+import { initGA, logPageView } from "../components/googleAnalytics.js"
+import { useEffect } from 'react'
+
 
 const MyApp = ({ Component, pageProps }) => {
+
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+      logPageView(window.location.pathname);
+    }
+  }, [])
+
   return (
     < > 
       {/* <Layout > */}
